5ce589f6cf9a15af56dc05d56b2b131c024f1102
NGBANDAJuste-Junior <njjngbanda20@yahoo.com>
2023-04-04 22:11:23 +0200

    fini la partie 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,36 @@
 
 
 function PokemonCard() {
+    
+    
+    const pokemonList = [
+        {
+          name: "bulbasaur",
+          sprite:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+          name: "mew",
+        },
+      ];
 
-    return (
-
-<div className="">
-    <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-
-        <figcaption>bulbasaur</figcaption>
-    </figure>
-</div>
-
-    );
+      // on cree une variable const du premier pokemon si l'on vet afficher le 2° pokemon
+   
+      const pokemon  = pokemonList [0]; // premier du tableau
+       // Si on veut afficher le deuxième pokémon :
+       // const pokemon = pokemonList[1];
+         
+      return (
+        <div>
+        <figure>
+          {pokemon.imgSrc ?
+            <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+          <figcaption>{pokemon.name}</figcaption>
+        </figure>
+      </div>
+      )
+      
+      
+  
 }
 export default PokemonCard;
